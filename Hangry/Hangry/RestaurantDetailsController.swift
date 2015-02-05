38a809f51527b83e445c2edd3025e07daad508af
Hannah Teuteberg <hannah.teuteberg@futurice.com>
2015-02-05 21:06:22 +0100

    crash fix

--- Hangry/Hangry/RestaurantDetailsController.swift
+++ Hangry/Hangry/RestaurantDetailsController.swift
@@ -22,7 +22,7 @@ class RestaurantDetailsController: UIViewController {
     }
 
     required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
 
     override func viewDidLoad() {
